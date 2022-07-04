589479550dbd0c9609a5e778a9d6adbc427d0eaf
Geezer4 <108690038+GeezerGitHub@users.noreply.github.com>
2022-07-04 19:34:10 +0200

    Add files via upload

---
+++ temporary arabmoneh.lua
@@ -0,0 +1,201 @@
+print("hi sourc")
+
+local frame = 1
+
+if frame == 1 and Name == "ArabMoneh" then
+Window:SetBackground("10108811944")
+wait()
+frame = frame+1
+end
+
+if frame == 2 and Name == "ArabMoneh" then
+Window:SetBackground("10109280289")
+wait()
+frame = frame+1
+end
+
+if frame == 3 and Name == "ArabMoneh" then
+Window:SetBackground("10109281132")
+wait()
+frame = frame+1
+end
+
+if frame == 4 and Name == "ArabMoneh" then
+Window:SetBackground("10109283437")
+wait()
+frame = frame+1
+end
+
+if frame == 5 and Name == "ArabMoneh" then
+Window:SetBackground("10109284375")
+wait()
+frame = frame+1
+end
+
+if frame == 6 and Name == "ArabMoneh" then
+Window:SetBackground("10109285027")
+wait()
+frame = frame+1
+end
+
+if frame == 7 and Name == "ArabMoneh" then
+Window:SetBackground("10109285746")
+wait()
+frame = frame+1
+end
+
+if frame == 8 and Name == "ArabMoneh" then
+Window:SetBackground("10109286557")
+wait()
+frame = frame+1
+end
+
+if frame == 9 and Name == "ArabMoneh" then
+Window:SetBackground("10109287726")
+wait()
+frame = frame+1
+end
+
+if frame == 10 and Name == "ArabMoneh" then
+Window:SetBackground("10109288607")
+wait()
+frame = frame+1
+end
+
+if frame == 11 and Name == "ArabMoneh" then
+Window:SetBackground("10109289383")
+wait()
+frame = frame+1
+end
+
+if frame == 12 and Name == "ArabMoneh" then
+Window:SetBackground("10109289998")
+wait()
+frame = frame+1
+end
+
+if frame == 13 and Name == "ArabMoneh" then
+Window:SetBackground("10109294775")
+wait()
+frame = frame+1
+end
+
+if frame == 14 and Name == "ArabMoneh" then
+Window:SetBackground("10109296927")
+wait()
+frame = frame+1
+end
+
+if frame == 15 and Name == "ArabMoneh" then
+Window:SetBackground("10109297527")
+wait()
+frame = frame+1
+end
+
+if frame == 16 and Name == "ArabMoneh" then
+Window:SetBackground("10109297943")
+wait()
+frame = frame+1
+end
+
+if frame == 17 and Name == "ArabMoneh" then
+Window:SetBackground("10109299347")
+wait()
+frame = frame+1
+end
+
+if frame == 18 and Name == "ArabMoneh" then
+Window:SetBackground("10109300076")
+wait()
+frame = frame+1
+end
+
+if frame == 19 and Name == "ArabMoneh" then
+Window:SetBackground("10109300791")
+wait()
+frame = frame+1
+end
+
+if frame == 20 and Name == "ArabMoneh" then
+Window:SetBackground("10109301690")
+wait()
+frame = frame+1
+end
+
+if frame == 21 and Name == "ArabMoneh" then
+Window:SetBackground("10109304578")
+wait()
+frame = frame+1
+end
+
+if frame == 22 and Name == "ArabMoneh" then
+Window:SetBackground("10109305636")
+wait()
+frame = frame+1
+end
+
+if frame == 23 and Name == "ArabMoneh" then
+Window:SetBackground("10109306493")
+wait()
+frame = frame+1
+end
+
+if frame == 24 and Name == "ArabMoneh" then
+Window:SetBackground("10109307675")
+wait()
+frame = frame+1
+end
+
+if frame == 25 and Name == "ArabMoneh" then
+Window:SetBackground("10109308753")
+wait()
+frame = frame+1
+end
+
+if frame == 26 and Name == "ArabMoneh" then
+Window:SetBackground("10109309708")
+wait()
+frame = frame+1
+end
+
+if frame == 27 and Name == "ArabMoneh" then
+Window:SetBackground("10109311025")
+wait()
+frame = frame+1
+end
+
+if frame == 28 and Name == "ArabMoneh" then
+Window:SetBackground("10109311790")
+wait()
+frame = frame+1
+end
+
+if frame == 29 and Name == "ArabMoneh" then
+Window:SetBackground("10109312688")
+wait()
+frame = frame+1
+end
+
+if frame == 30 and Name == "ArabMoneh" then
+Window:SetBackground("10109313748")
+wait()
+frame = frame+1
+end
+
+if frame == 31 and Name == "ArabMoneh" then
+Window:SetBackground("10109315328")
+wait()
+frame = frame+1
+end
+
+if frame == 32 and Name == "ArabMoneh" then
+Window:SetBackground("10109315941")
+wait()
+frame = frame+1
+end
+
+if frame == 33 and Name == "ArabMoneh" then
+Window:SetBackground("10109316753")
+wait()
+frame = frame+1
+end
